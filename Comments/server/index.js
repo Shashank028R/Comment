d856a6b6cd28e4febb55708c9db80c350657ee02
Shashank Kumar <shashankmuz3@gmail.com>
2026-04-26 13:43:18 +0530

    deploy

--- Comments/server/index.js
+++ Comments/server/index.js
@@ -7,7 +7,9 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 app.use(express.json());
 app.use('/api', userRoutes);
